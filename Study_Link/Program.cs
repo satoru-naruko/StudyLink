30b5d252a5e3271a384ba6e10e5d99f736b214d6
鳴子智 <narukosatoru@MacBook-Pro.local>
2017-08-06 00:15:32 +0900

    Link学習用プロジェクトをコミット

---
+++ Study_Link/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Study_Link
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            int[] list = new int[] { 1, 23, 4, 56, 78, 9, 999 };
+
+            var tmp = list.Where(x => x >= 10).Select(x => x.ToString()+"naruko");
+
+			foreach (var test in tmp )
+            {
+                Console.WriteLine(test);
+            }
+        }
+    }
+}
