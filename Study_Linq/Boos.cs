07f40f4d3f6d5f01ebf088776f81591096c2c848
鳴子智 <naruko505@gmail.com>
2019-04-14 15:41:56 +0900

    別ソリューションの内容をマージ

---
+++ Study_Linq/Boos.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Study_Linq
+{
+    public class Book
+    {
+        public string Tiele{ get; set; }
+        public int Price{ get; set; }
+    }
+
+    public class BookRepository
+    {
+        public IEnumerable<Book> GetBook()
+        {
+            return new List<Book>
+            {
+                new Book(){Tiele = "aasfaaa", Price=3 },
+                new Book(){Tiele = "aacZcsfaaa", Price=30 },
+                new Book(){Tiele = "sadfaaa", Price=311 },
+                new Book(){Tiele = "aafaa", Price=13 },
+                new Book(){Tiele = "asasafasfdffaaaaaa", Price=53 },
+
+            };
+        }
+
+    }
+}
--- Study_Linq/Program.cs
+++ Study_Linq/Program.cs
@@ -1,21 +1,52 @@
 ﻿using System;
 using System.Linq;
 
-namespace Study_Link
+namespace Study_Linq
 {
     class MainClass
     {
-        public static void Main(string[] args)
+        private static readonly string split_str = "---------------------------------";
+        static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
 
-            int[] list = new int[] { 1, 23, 4, 56, 78, 9, 999 };
+            var books = new BookRepository().GetBook();
 
-            var tmp = list.Where(x => x >= 10).Select(x => x.ToString()+"naruko");
-
+            try
             {
-                Console.WriteLine(test);
+                // 抽出
+                var cheapBooks = books.Where(b => b.Price < 30).OrderBy(b => b.Tiele).Select(b => b.Tiele);
+                foreach (var item in cheapBooks)
+                {
+                    Console.WriteLine(item);
+                }
+                Console.WriteLine(split_str);
+
+                // 抽出/加工
+                var list = books.Select(b => b.Tiele + "_naruko");
+                foreach (var appendstr in list)
+                {
+                    Console.WriteLine(appendstr);
+                }
+                Console.WriteLine(split_str);
+
+                // 検索
+                var book = books.SingleOrDefault(b => b.Tiele == "aasfaaa");
+                Console.WriteLine(book.Tiele + " is only");
+                Console.WriteLine(split_str);
+
+                // 値段10以上の合計
+                var prices = books.Where(b => b.Price > 10).Sum(b => b.Price);
+                Console.WriteLine(prices);
             }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
         }
     }
 }
